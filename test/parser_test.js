13ebec81bddeef682fa7c66165ccfa4c9066368a
Chris Papazian <chris@vessel.com>
2015-10-30 22:12:29 -0700

    handle branches

--- test/parser_test.js
+++ test/parser_test.js
@@ -8,44 +8,63 @@ let variationSGF = require("../sgf/variation.sgf");
 
 describe('parser', () => {
   describe('parse', () => {
-    it('parses without errors', () => {
+    it('parses a simple SGF', () => {
       expect(() => parser.parse(simpleSGF)).to.not.throw();
     });
 
-    it('parses a simple SGF', () => {
-      let parsed = [
-        {FF: 4, GM: 1, SZ: 19},
-        {B: "aa"},
-        {W: "bb"},
-        {B: "cc"},
-        {W: "dd"},
-        {B: "ad"},
-        {W: "bd"}
-      ];
-      expect(parser.parse(simpleSGF)).to.eql(parsed);
+    it('returns the root node', () => {
+      let root = parser.parse(simpleSGF);
+      expect(root.FF).to.equal(4);
+      expect(root.GM).to.equal(1);
+      expect(root.SZ).to.equal(19);
+      expect(root._next).to.be.an('object');
+    });
+
+    it('root node has pointer to next', () => {
+      let root = parser.parse(simpleSGF);
+      let child = root._next;
+      expect(child.B).to.equal("aa");
+    });
+
+    it('child node has pointer to parent', () => {
+      let root = parser.parse(simpleSGF);
+      let child = root._next;
+      expect(child._parent).to.eql(root);
     });
 
     it('parses an SGF with branches', () => {
-      // let parsed = [
-        // {FF: 4, GM: 1, SZ: 19},
-        // {B: "aa"},
-        // {W: "bb"},
-        // [
-        //   {B: "cc", N: "Var A"}
-        // ]
-        // {B: "cc"},
-        // {W: "dd"},
-        // {B: "ad"},
-        // {W: "bd"}
-        //
-        // (;FF[4]GM[1]SZ[19];B[aa];W[bb](;B[cc]N[Var A];W[dd];B[ad];W[bd])
-        // (;B[hh]N[Var B];W[hg])
-        // (;B[gg]N[Var C];W[gh];B[hh];W[hg];B[kk]))
-      // ]
-      expect(() => {
-        var result = parser.parse(variationSGF);
-        console.log(inspect(result, {depth: null}));
-      }).to.not.throw();
+      expect(() => parser.parse(variationSGF)).to.not.throw();
     });
+
+    it('has a pointer to the main branch', () => {
+      let root = parser.parse(variationSGF);
+      let mainline = root._next._next._next;
+      expect(mainline.B).to.equal("cc");
+      expect(mainline.N).to.equal("Var A");
+    });
+
+    it('main branch has a pointer to parent', () => {
+      let root = parser.parse(variationSGF);
+      let main = root._next._next;
+      let main_branch = main._next;
+      expect(main_branch._parent).to.eql(main);
+    });
+
+    it('has a pointer to variations', () => {
+      let root = parser.parse(variationSGF);
+      let variations = root._next._next._variations;
+      expect(variations[0].B).to.equal("hh");
+      expect(variations[0].N).to.equal("Var B");
+      expect(variations[1].B).to.equal("gg");
+      expect(variations[1].N).to.equal("Var C");
+    });
+
+    it('variations have pointers to parent', () => {
+      let root = parser.parse(variationSGF);
+      let main = root._next._next;
+      let variations = main._variations;
+      expect(variations[0]._parent).to.eql(main);
+      expect(variations[1]._parent).to.eql(main);
+    })
   });
 });
