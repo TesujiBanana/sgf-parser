b726d74c83d9668d93e1c78e90ecd783365ad48d
Chris Papazian <chris@vessel.com>
2015-11-02 21:01:06 -0800

    don't inject empty main module

--- src/generate.js
+++ src/generate.js
@@ -4,6 +4,6 @@ var parser = require("./parser");
 var fs = require("fs");
 
 var location = "lib/parser.js";
-var contents = parser.generate({moduleMain: function() {}});
+var contents = parser.generate();
 
 fs.writeFileSync(location, contents);
