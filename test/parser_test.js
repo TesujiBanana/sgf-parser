91844a54d9c95d2e0d085eb3a0f70c13457877e7
Chris Papazian <chris@vessel.com>
2015-11-08 18:32:07 -0800

    return list of all nodes

--- test/parser_test.js
+++ test/parser_test.js
@@ -15,22 +15,33 @@ describe('parser', () => {
       expect(() => parser.parse(simpleSGF)).to.not.throw();
     });
 
-    it('returns the root node', () => {
-      var root = parser.parse(simpleSGF);
+    it('returns a list of nodes', () => {
+      var nodes = parser.parse(simpleSGF);
+      expect(nodes).to.be.instanceof(Array);
+    });
+
+    it('root node is first', () => {
+      var root = parser.parse(simpleSGF)[0];
       expect(root.FF).to.equal(4);
       expect(root.GM).to.equal(1);
       expect(root.SZ).to.equal(19);
       expect(root._next).to.be.an('object');
     });
 
+    it('last node is last', () => {
+      var nodes = parser.parse(simpleSGF);
+      var last = nodes[nodes.length - 1];
+      expect(last.W).to.equal('bd');
+    });
+
     it('root node has pointer to next', () => {
-      var root = parser.parse(simpleSGF);
+      var root = parser.parse(simpleSGF)[0];
       var child = root._next;
       expect(child.B).to.equal("aa");
     });
 
     it('child node has pointer to parent', () => {
-      var root = parser.parse(simpleSGF);
+      var root = parser.parse(simpleSGF)[0];
       var child = root._next;
       expect(child._parent).to.eql(root);
     });
@@ -40,7 +51,7 @@ describe('parser', () => {
     });
 
     it('has a pointer to variations', () => {
-      var root = parser.parse(variationSGF);
+      var root = parser.parse(variationSGF)[0];
       var variations = root._next._next._variations;
       expect(variations[0].B).to.equal("cc");
       expect(variations[0].N).to.equal("Var A");
@@ -51,7 +62,7 @@ describe('parser', () => {
     });
 
     it('variations have pointers to parent', () => {
-      var root = parser.parse(variationSGF);
+      var root = parser.parse(variationSGF)[0];
       var main = root._next._next;
       var variations = main._variations;
       expect(variations[0]._parent).to.eql(main);
@@ -60,19 +71,19 @@ describe('parser', () => {
     });
 
     it('nodes can have no (zero) properties', () => {
-      var root = parser.parse(problemSGF);
+      var root = parser.parse(problemSGF)[0];
       var emptyNode = _.omit(root._next, "_parent", "_next", "_variations");
       expect(emptyNode).to.eql({});
     });
 
     it('properties can have multiple values', () => {
-      var root = parser.parse(problemSGF);
+      var root = parser.parse(problemSGF)[0];
       expect(root.AB).to.eql(["ob", "nc", "pc", "qc", "qe", "re"]);
       expect(root.AW).to.eql(["pb", "qb", "rb", "rc", "rd"]);
     });
 
     it('comments can be multiples lines', () => {
-      var root = parser.parse(commentSGF);
+      var root = parser.parse(commentSGF)[0];
       expect(root.C).to.match(/multi-/);
       expect(root.C).to.match(/line/);
       expect(root.C).to.match(/comment!!!/);
